47c08faf0ad1ed32228997f20aa0800418fd21fa
Jimmy-O-Joel <jimmyjoel177@gmail.com>
2023-07-14 18:00:50 +0300

    implement execute

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 //main.c
 #include "shell.h"
 
+/* Built-in command names and corresponding functions */
+
+const char *builtin_str[] = {"cd","exit\n","help\n"};
+
+int (*builtin_func[]) (char **) = {
+    &cd_shell
+};
+
 /**
 *main - entry point of our program
 *
@@ -8,9 +16,9 @@
 */
 
 
-int main(void)
+int main(int argc, char **argv)
 {
-	Prompt();
+	prompt();
 	return (0);
 
 }
@@ -25,7 +33,7 @@ int main(void)
 
 void prompt(void)
 {
-	const char *prompt = "$"
+	const char *prompt = "$";
 	ssize_t count = 0;
 	char * line;
 	char **args;
@@ -42,8 +50,8 @@ void prompt(void)
 		}
 
 		line = read_line();
-		args = parser (line);
-		status = execute (args);
+		args = parser(line);
+		status = execute(args);
 
 		free(line);
 		free(args);
@@ -93,13 +101,14 @@ char *read_line(void)
 *@Return: pointer to an array
 */
 char **parser(char *line)
+{
 
-	int buffer_size = 1024;
+	int buf_size = TOK_BUFSIZE;
 	int i = 0;
 	char **token_holder;
 	char *token;
 	
-	token_holder = (char**)malloc(buffer_size * sizeof(char *));
+	token_holder = (char**)malloc(buf_size * sizeof(char *));
 	if (token_holder == NULL)
 	{
 		return (NULL);
@@ -110,21 +119,96 @@ char **parser(char *line)
 	{
 		token_holder[i] = token;
 		i++;
-		if(i >= buffer_size)
+		if(i >= buf_size)
 		{
-			buffer_size +=buffer_size;
-			token_holder = realloc(token_holder, buffer_size *sizeof(char*));
+			buf_size +=buf_size;
+			token_holder = realloc(token_holder, buf_size *sizeof(char*));
 			if (token_holder == NULL)
 			{
 				return (NULL);
 			}
 		}
-		token = strok(NULL, " ");
+		token = strtok(NULL, " ");
 	}
 	
 	token_holder[i] = NULL;
-	return (token_holder)
+	return (token_holder);
 }
+
+
+
+/**
+ * cd_shell - change to another directory
+ *
+ * @datash: data relevant (status and args)
+ * Return: 0 on success.
+ */
+int cd_shell(char **args)
+{
+  if (args[1] == NULL) {
+    fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+  } else {
+    if (chdir(args[1]) != 0) {
+      perror("lsh");
+    }
+  }
+  return 1;
+}
+
+/**
+ * exit_shell - exits the shell
+ *
+ * @datash: data relevant (status and args)
+ * Return: 0 on success.
+ */
+ 
+int exit_shell(char **args)
+{
+  return 0;
+}
+
+/**
+*launch_shell - launches the shell
+*@args: array of argument strings
+*Return: always success
+*/
+
+int launch_shell(char **args)
+{
+    pid_t pid, wpid;
+    int status;
+    
+    pid = fork();
+    
+    if (pid == 0)
+    {
+        /* Child process */
+        if (execv(args[0], args) == -1)
+        {
+            perror("lsh");
+        }
+        exit(EXIT_FAILURE);
+    }
+    else if (pid < 0)
+    {
+        /* Error forking */
+        perror("lsh");
+    }
+    else
+    {
+        /* Parent process */
+        do
+        {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        }
+        while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+    
+    return 1;
+}
+		
+
+
 
 //execute.c
 
@@ -136,7 +220,23 @@ char **parser(char *line)
 
 int execute(char **args)
 {
-	pid_t pid;
-	int status;
 
-	pid = fork();
+	int i;
+	int equal;
+	
+    if (args[0] == NULL)
+    {
+        return 1;  /* Empty command */
+    }
+    
+    for (i = 0; i < sizeof(builtin_str) / sizeof(char *); i++)
+    {
+        if (strcmp(builtin_str[i], args[0]) == 0)
+        {
+            return (*builtin_func[i])(args);
+        }
+
+    }
+    
+    return launch_shell(args);
+}
--- shell.h
+++ shell.h
@@ -1,7 +1,7 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-/--HEADER FILES--/
+/*--HEADER FILES--*/
 
 #include <sys/wait.h>
 #include <unistd.h>
@@ -9,11 +9,17 @@
 #include <stdio.h>
 #include <string.h>
 
+#define TOK_BUFSIZE 1024
 
-/--FUNCTION PROTOTYPES--/
+
+/*--FUNCTION PROTOTYPES--*/
 void prompt(void);
 char *read_line(void);
 char **parser(char *line);
 int execute(char **args);
+int get_builtin(char *cmd);
+int exit_shell(char **args);
+int cd_shell(char **args);
+int get_help(char **args);
 
 #endif
